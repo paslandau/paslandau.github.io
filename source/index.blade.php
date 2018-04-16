0b88f2cea82665d9171902bc4ff4b6239f523699
Pascal Landau <pascal.landau@googlemail.com>
2018-04-17 00:34:19 +0200

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -14,5 +14,5 @@ $description = "Homepage of Pascal Landau";
 <p>Greetings, dear stranger of the internet. Please take a seat and make yourself at home. To shorten your waiting time, you
 might enjoy a little reading over at the <a href="/blog">blog</a> or the <a href="/bigquery-snippets">BigQuery snippets</a>.
     That is, if you're interested in coding/software development, working out or simply in what I have to write.</p>
-<p>In case we never met and you have no idea who you're dealing with, thou shalt start at the <a href="/about">about moi</a> page.</p>
+<p>In case we never met and you have no idea who you're dealing with, thou shalt start at the <a href="/about">about me</a> page.</p>
 @endsection
